cf195478e237665484fa4f18ed94c8fbfb5f7912
alemedellin <ale.medellin@live.com.mx>
2021-04-23 13:03:50 -0500

    catalogo de clientes

--- combucar-front/src/app/components/catalogos/clientes/clientes.component.ts
+++ combucar-front/src/app/components/catalogos/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  clientesForm: FormGroup = this.fb.group({
+    nombre:         ['', Validators.required, Validators.maxLength(150)],
+    rfc:            ['', Validators.required, Validators.maxLength(45)],
+    identificacion: ['', Validators.required, Validators.maxLength(45)],
+    telefono:       ['', Validators.required, Validators.maxLength(45)],
+    correo:         ['', Validators.required, Validators.maxLength(45)]
+  });
+
+  clientes : Cliente[] = [
+    {
+      nombre: 'Alberto Navarrete Garcia',
+      rfc: 'RFC000101ABC',
+      identificacion: '123456789',
+      telefono: '55 5555 5555',
+      correo: 'alberto@combucar.com',
+    },
+    {
+      nombre: 'Jonathan De La Torre Díaz',
+      rfc: 'RFC000202ABC',
+      identificacion: '234567890',
+      telefono: '55 3333 3333',
+      correo: 'jonathan@combucar.com',
+    }
+  ];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  campoEsValido( campo: string) {
+    return this.clientesForm.controls[campo].errors && 
+           this.clientesForm.controls[campo].touched
+  }
+
+  guardar(){
+    // if(this.clientesForm.invalid){
+    //   return Object.values(this.clientesForm.controls).forEach(control => {
+    //     control.markAsTouched();
+    //   });
+    // }
+
+    let cliente: Cliente = {
+      nombre: this.clientesForm.get('nombre')?.value,
+      rfc: this.clientesForm.get('rfc')?.value,
+      identificacion: this.clientesForm.get('identificacion')?.value,
+      telefono: this.clientesForm.get('telefono')?.value,
+      correo: this.clientesForm.get('correo')?.value
+    }
+
+    this.clientes.push(cliente);
+    window.alert('Cliente Guardado Exitosamente');
+    this.clientesForm.reset();
+  }
+
 }
+
+interface Cliente{
+  nombre: string,
+  rfc: string,
+  identificacion: string,
+  telefono: string,
+  correo: string
+};
